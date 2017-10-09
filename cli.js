a7da80b943cf7adb969818d507656e22d485d000
elzup <guild0105@gmail.com>
2017-10-09 18:59:31 +0900

    Fix paths

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 'use strict'
 
 const fs = require('fs')
-const toshino = process.cwd() + '/toshino.out'
+const toshino = __dirname + '/toshino.out'
 const data = fs.readFileSync(toshino, 'utf-8')
 
 console.log(data)
